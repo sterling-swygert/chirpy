afd7ef284bc9b4283bc54b31fdf551068d200981
sterling-swygert <sterling.swygert@gmail.com>
2025-11-21 22:44:14 -0500

    (feat): Added some endpoints

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/sterling-swygert/mystrings
+
+go 1.24.3
---
+++ main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+	"sync/atomic"
+)
+
+type apiConfig struct{
+		fileserverHits atomic.Int32
+	}
+
+func healthHandler(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	writer.WriteHeader(200)
+	writer.Write([]byte("OK"))
+}
+
+func (cfg *apiConfig) hitsHandler(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	writer.WriteHeader(200)
+	hits := strconv.Itoa(int(cfg.fileserverHits.Load()))
+	writer.Write([]byte("Hits: " + hits))
+	fmt.Println("Hits: " + hits)
+}
+
+func (cfg *apiConfig) resetHitsHandler(writer http.ResponseWriter, request *http.Request) {
+	writer.Header().Add("Content-Type", "text/html; charset=utf-8")
+	writer.WriteHeader(200)
+	cfg.fileserverHits.Store(0)
+	hits := Itoa(int(cfg.fileserverHits.Load()))
+	fmt.Sprintf("<html>
+  <body>
+    <h1>Welcome, Chirpy Admin</h1>
+    <p>Chirpy has been visited %d times!</p>
+  </body>
+</html>", hits)
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+
+func main() {
+	fmt.Println("Hello World")
+	cfg := &apiConfig{}
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /api/healthz", healthHandler)
+	mux.HandleFunc("GET /admin/metrics", cfg.hitsHandler)
+	mux.HandleFunc("POST /admin/reset", cfg.resetHitsHandler)
+	handler := http.StripPrefix("/app", http.FileServer(http.Dir("./app/")))
+	mux.Handle("/app/", cfg.middlewareMetricsInc(handler))
+	server := http.Server{
+		Addr: ":8080",
+		Handler: mux,
+	}
+	server.ListenAndServe()
+}
